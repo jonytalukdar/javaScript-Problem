b739597fc1ae4c5398a93cb771596fdd8a77c74c
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-20 21:40:10 +0600

    added some line in random_number.js

---
+++ random_number.js
@@ -0,0 +1,22 @@
+// while (true) {
+//   var rand = Math.floor(Math.random() * 15 + 1);
+//   if (rand == 12) {
+//     console.log('Winner Winner Chiken Dinner');
+//     break;
+//   } else {
+//     console.log('You have entered : ', rand);
+//   }
+// }
+
+// var num = 12.83123;
+// var floored = Math.floor(num);
+// var ceiled = Math.ceil(num);
+// var rounded = Math.round(num);
+// var randomed = Math.floor(Math.random() * 6 + 1);
+// console.log(floored, ceiled, rounded);
+// console.log(randomed);
+
+for (var i = 0; i <= 6; i++) {
+  var num = Math.floor(Math.random() * 6 + 1);
+  console.log(num);
+}
