f51ccb3b4c309c7cdf80df861aae799a54a12cb2
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-21 22:05:12 +0600

    added some line in travelling.js

---
+++ travelling.js
@@ -0,0 +1,39 @@
+let depth = 22;
+var animal = 1;
+if (depth <= 10) {
+  animal = depth * 50;
+} else if (depth <= 20) {
+  var firstPart = 10 * 50;
+  var remaining = depth - 10;
+  var secondPart = remaining * 100;
+  animal = firstPart + secondPart;
+} else {
+  var firstPart = 10 * 50;
+  var secondPart = 10 * 100;
+  var remaining = depth - 20;
+  var thirdPart = remaining * 300;
+  var animal = firstPart + secondPart + thirdPart;
+}
+console.log(animal);
+
+function travellingWild(depth) {
+  var animal = 1;
+  if (depth <= 10) {
+    animal = depth * 50;
+  } else if (depth <= 20) {
+    var firstPart = 10 * 50;
+    var remaining = depth - 10;
+    var secondPart = remaining * 100;
+    animal = firstPart + secondPart;
+  } else {
+    var firstPart = 10 * 50;
+    var secondPart = 10 * 100;
+    var remaining = depth - 20;
+    var thirdPart = remaining * 300;
+    var animal = firstPart + secondPart + thirdPart;
+  }
+  return animal;
+}
+
+var result = travellingWild(22);
+console.log(result);
