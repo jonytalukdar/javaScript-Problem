befb7217203973e1a901d502569441467c9450e3
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-21 21:27:02 +0600

    modify recursion.js

--- recursionWork.js
+++ recursionWork.js
@@ -1,3 +1,18 @@
+// using for loop
+
+function sample(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+var result2 = sample(5);
+console.log(result2);
+
+// using recursive way
+
 function factorial(n) {
   if (n == 1) {
     console.log(n);
@@ -10,14 +25,3 @@ function factorial(n) {
 
 var result = factorial(5);
 console.log(result);
-
-function sample(num) {
-  let fact = 1;
-  for (let i = 1; i <= num; i++) {
-    fact *= i;
-  }
-  return fact;
-}
-
-var result2 = sample(5);
-console.log(result2);
