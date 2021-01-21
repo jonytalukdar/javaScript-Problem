2d5dcca9d2483b37a16b9587e30e75c1015ef7ce
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-21 21:25:46 +0600

    added some line in recursion.js file

---
+++ recursionWork.js
@@ -0,0 +1,23 @@
+function factorial(n) {
+  if (n == 1) {
+    console.log(n);
+    return 1;
+  } else {
+    console.log(n, n - 1);
+    return n * factorial(n - 1);
+  }
+}
+
+var result = factorial(5);
+console.log(result);
+
+function sample(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+var result2 = sample(5);
+console.log(result2);
