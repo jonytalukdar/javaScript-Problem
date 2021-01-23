334b2b8207b7522e3c51fb91a8c833b392485f82
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-23 21:11:10 +0600

    added some line in object vs array file

---
+++ object_vs_array.js
@@ -0,0 +1,14 @@
+let myself1 = ['joney talukdar', 21, 45, 'blue'];
+let friends = ['joney', 'shimul', 'akash', 'rashedul', 'robin', 'alhaz'];
+
+let myself = {
+  name: 'Joney Talukdar',
+  age: 21,
+  weight: 48,
+  favouriteColor: 'blue',
+};
+
+console.log(myself.age);
+console.log(myself['age']);
+let lookingForAge = 'age';
+console.log(myself[lookingForAge]);
